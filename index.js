a05830615daca0e6af88d7667314e19bacce24e7
alexander aivars <alex@kramgo.se>
2014-04-11 13:22:33 +0200

    added baseurl as a config option

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+var path = require('path');
+
 var createReconfigPreprocessor = function(args, config, logger, helper) {
   config = config || {};
 
-  var log = logger.create('preprocessor.reconfig');
-  var options = args.options || config.options || {};
+  var log = logger.create('preprocessor.reconfig'),
+			options = args.options || config.options || {
+				baseUrl: '/base'
+			};
 
   return function(content, file, done) {
     log.debug('Processing "%s".', file.originalPath);
-
     var result;
     try {
       // ToDo: add config options and make more general purpose
 			log.debug('Processing "%s".', file.originalPath);
-			var base = /(baseUrl)\s*:\s*("|')(.*)\2/gi;
-			var bower = /\/bower_components/gi; 
-			result = content.replace(base, '//').replace(bower, 'bower_components');
-      done(result);
+			var base = /(baseUrl\s*:\s*)("|')(.*)\2/gi;
+			result = content.replace(base, '$1$2'+options.baseUrl+'$2');
+			done(result);
     } catch (error) {
       log.error('%s\n  at %s', error.message, file.originalPath);
       done(content);
